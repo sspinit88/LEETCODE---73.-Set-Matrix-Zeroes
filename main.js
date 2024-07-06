19220caa5cd457849c8cb749f14fbe27e4983bc8
sspinit88 <39233740+sspinit88@users.noreply.github.com>
2024-07-06 18:59:27 +0400

    Update main.js

--- main.js
+++ main.js
@@ -39,42 +39,55 @@ Here are the steps we will follow:
 4. If the first element of the matrix is 0, set the entire first row to 0.
 5. If the first element in the first column is 0, set the entire first column to 0.
 */
-
 function setZeroes(matrix) {
-  let isCol = false;
-  let R = matrix.length;
-  let C = matrix[0].length;
+    // Инициализируем переменную для отслеживания, должен ли первый столбец быть установлен в 0
+    // Initialize a variable to track whether the first column should be set to 0
+    let isCol = false;
+    let R = matrix.length;
+    let C = matrix[0].length;
 
-  for (let i = 0; i < R; i++) {
-    if (matrix[i][0] == 0) {
-      isCol = true;
-    }
-
-    for (let j = 1; j < C; j++) {
-      if (matrix[i][j] == 0) {
-        matrix[0][j] = 0;
-        matrix[i][0] = 0;
-      }
-    }
-  }
-
-  for (let i = 1; i < R; i++) {
-    for (let j = 1; j < C; j++) {
-      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-        matrix[i][j] = 0;
-      }
-    }
-  }
-
-  if (matrix[0][0] == 0) {
-    for (let j = 0; j < C; j++) {
-      matrix[0][j] = 0;
-    }
-  }
-
-  if (isCol) {
     for (let i = 0; i < R; i++) {
-      matrix[i][0] = 0;
+        // Если первый элемент в строке равен 0, устанавливаем isCol в true
+        // If the first element in the row is 0, set isCol to true
+        if (matrix[i][0] == 0) {
+            isCol = true;
+        }
+
+        // Проходим по каждому элементу в строке, начиная со второго
+        // Go through each element in the row, starting from the second
+        for (let j = 1; j < C; j++) {
+            // Если элемент равен 0, устанавливаем соответствующий элемент в первой строке и первом столбце в 0
+            // If the element is 0, set the corresponding element in the first row and the first column to 0
+            if (matrix[i][j] == 0) {
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
+            }
+        }
     }
-  }
+
+    // Проходим по каждой строке, начиная со второй, и устанавливаем элементы в 0, если соответствующий элемент в первой строке или первом столбце равен 0
+    // Go through each row, starting from the second, and set elements to 0 if the corresponding element in the first row or the first column is 0
+    for (let i = 1; i < R; i++) {
+        for (let j = 1; j < C; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Если первый элемент матрицы равен 0, устанавливаем всю первую строку в 0
+    // If the first element of the matrix is 0, set the entire first row to 0
+    if (matrix[0][0] == 0) {
+        for (let j = 0; j < C; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Если isCol равен true, устанавливаем весь первый столбец в 0
+    // If isCol is true, set the entire first column to 0
+    if (isCol) {
+        for (let i = 0; i < R; i++) {
+            matrix[i][0] = 0;
+        }
+    }
 }
